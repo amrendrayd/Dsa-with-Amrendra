55c9aef247546d661d355b4febd4c4725c7d303b
amrendrayd <amrendrayd3@gmail.com>
2024-07-30 21:35:29 +0530

    Solved Short 0s

---
+++ vector/short01.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector <int> arr{0,1,0,1,0,1,1,0,0,1,1};
+
+    int start = 0;
+    int end = arr.size() - 1;
+    int i = 0;
+
+    while(i != end){
+        if(arr[i] == 0) {
+            swap(arr[start], arr[i]);
+            start++;
+            i++;
+        }
+        else {
+            swap(arr[end], arr[i]);
+            end--;
+        }
+    }
+
+    for(auto value: arr){
+        cout << value << " ";
+    }
+}
