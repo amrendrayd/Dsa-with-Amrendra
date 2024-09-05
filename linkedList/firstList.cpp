2cb036dc701798971c4200a79920c4ecc3a4d774
amrendrayd <amrendrayd3@gmail.com>
2024-09-05 23:04:58 +0530

    Learning linked List

---
+++ linkedList/firstList.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* next;
+    
+        // constructor
+        Node() {
+            this->data = 0;
+            this->next = NULL;
+        }
+
+        Node(int data) {
+            this->data = data;
+            this->next = NULL;
+        }
+};
+
+void print(Node* &head) {
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main() {
+    // new node create
+    Node* first = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+    Node* fourth = new Node(40);
+    Node* fifth = new Node(50);
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+
+    print(first);
+  
+
+
+    return 0;
+}
+
